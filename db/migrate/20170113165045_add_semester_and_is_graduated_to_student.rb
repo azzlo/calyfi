b9081b0528b4625ccb903195a7f40ddb87a42065
Oscar Tellez <oscartzgz@gmail.com>
2017-10-07 02:15:11 -0500

    Migrations removed not affected db

--- db/migrate/20170113165045_add_semester_and_is_graduated_to_student.rb
+++
@@ -1,6 +0,0 @@
-class AddSemesterAndIsGraduatedToStudent < ActiveRecord::Migration[5.0]
-  def change
-    add_column :students, :semester, :integer
-    add_column :students, :is_graduated, :boolean
-  end
-end
